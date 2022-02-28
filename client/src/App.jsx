ef0de64d939d2d0a4b6412a483e85d8757fa2be4
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2022-02-28 21:14:09 +0100

    Add Tailwind CSS to the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,9 @@ function App() {
 
   return (
     <div className="App">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
