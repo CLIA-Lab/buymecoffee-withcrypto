fa53b78a4e530fd6f18dfccbdaff7a39ed752529
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2022-03-02 01:29:50 +0100

    Section 2 and 3 were added, now they are responsive

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,6 +1,12 @@
 const Welcome = () => {
     return (
-        <h1>Welcome</h1>
+      <div className='flex flex-col justify-between bg-no-repeat bg-top bg-[url(../../images/SectionOne.jpg)] 4k:bg-[#000022] h-[27.50rem]'>
+          <div className='container mx-auto'>
+            <div className='flex justify-center pt-[18rem]'>
+                <button className='border-none rounded-lg text-2xl w-40 h-14 bg-sky-500/75 text-white'>Get Started</button>
+            </div>
+        </div>   
+      </div>
     );
 }
 export default Welcome;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Welcome} from './Welcome';
 export { default as Navbar} from './Navbar';
+export { default as AboutUs} from './AboutUs';
