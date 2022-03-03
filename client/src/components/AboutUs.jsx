c78c8993e364906b84f998e3ebacd8a2c95c3ea1
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2022-03-03 22:09:13 +0100

    Add CodeModalComponent Section to the project and created two branck in the git repositorie

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -10,7 +10,7 @@ const Welcome = () => {
                 <p className='mt-[1rem] mobile-S-Laptop:px-[1.50rem] text-xl'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In, error officiis! Nostrum aliquam perferendis qui. Suscipit, reiciendis voluptatibus eaque ullam delectus, accusamus nobis quidem culpa labore, ducimus recusandae magnam cum?</p>
             </div>
             <div className='laptop-4k:w-1/2  flex justify-center items-center'>
-                <img className='mobile-S-Laptop:px-[2rem] laptop-4k:w-[80%] laptop-4k:h-[80%]' src={SectionTwoImg} alt='Section Two Image'/>
+                <img className='mobile-S-Laptop:px-[2rem] laptop-4k:w-[80%] laptop-4k:h-[70%]' src={SectionTwoImg} alt='Section Two Image'/>
             </div>
         </div>
         );
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Welcome} from './Welcome';
 export { default as Navbar} from './Navbar';
 export { default as AboutUs} from './AboutUs';
 export { default as Technology} from './Technology';
+export { default as CodeModalComponent} from './CodeModalComponent';
