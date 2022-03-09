506bdb23b256b88d8a122355ad3667e6769526e5
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2022-03-09 22:14:23 +0100

    Minimal changes

--- client/src/components/stepConst.js
+++ client/src/components/stepConst.js
@@ -1,22 +1,13 @@
-import React from 'react';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+function variable(){
 
-class Steps extends React.Component{
-    constructor(props){
-        super(props);
-       
-        var step = ``;
-       
-        if(this.props.stepTitle == "Step 1"){
-            step = `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>`;
-        }else if(this.props.stepTitle == "Step 2"){
-            step = `<div class="d-grid gap-2 d-md-flex justify-content-md-end fixed-bottom">
+
+    const step1= `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>`;
+    const step2= `<div class="d-grid gap-2 d-md-flex justify-content-md-end fixed-bottom">
     <button class="rounded-circle" style="outline: none; border: none;" data-bs-toggle="modal" data-bs-target="#ModalFormCrypto" data-bs-whatever="@mdo">
         <img type="button" src="https://www.buymeacoffee.com/assets/img/guidelines/logo-mark-3.svg" class="img-thumbnail rounded-circle mx-auto" style="width: 4rem ; height: 4rem;" alt="NothingToShow">
     </button>
 </div>`;
-        }else if(this.props.stepTitle == "Step 3"){
-            step= `<div class="modal fade" id="ModalFormCrypto" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    const step3 = `<div class="modal fade" id="ModalFormCrypto" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -52,35 +43,8 @@ class Steps extends React.Component{
         </div>
     </div>
 </div>`;
-        }else if(this.props.stepTitle == "Step 4"){
-            step = `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>`;
-        }
-        this.state = {
-            preText: step,
-            copied: false,
-            value: 'Copy'
-          };
-    }
-    render(){
-        const gradient = 'bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500';
-        return(
-            <>
-                <p className='font-bold pt-6'>{this.props.stepTitle}</p>
-                <p className='pt-1'>{this.props.stepInfo}</p>
-                    
-                <div className={`${gradient} w-[95%]  mt-4 rounded-lg select-all hover:overflow-x-scroll overflow-y-hidden`}>
-                    <pre className="text-white text-lg py-2">{this.state.preText}</pre>
-                </div>
-                <div className='flex justify-end w-[95%]'>
-                    <CopyToClipboard text={this.state.preText}
-                    onCopy={() => this.setState({copied: true})}>
-                        <button className='border-none rounded-lg mt-3 text-lg w-[5rem] h-[2rem] bg-sky-500/75 text-white'
-                        onClick={() => this.setState({value: 'Copied'})}
-                        >{this.state.value}</button>             
-                    </CopyToClipboard>
-                </div>
-            </>
-        );
-    }
+    const step4 =  step = `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>`;
+
+    const stepArr =[step1, step2, step3, step4];
+    return stepArr;
 }
-export default Steps;
